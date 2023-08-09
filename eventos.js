7993b456158ee01158d30e4ba2308448c1e7d59b
AlejandroSacco <alejandroivan2001@gmail.com>
2023-08-08 22:07:56 -0300

    Sin doble alerta

--- eventos.js
+++ eventos.js
@@ -1,3 +1,7 @@
 document.getElementById('contenedor').addEventListener('click', function(){
     alert("Hola! Soy el div")
 });
+
+document.getElementById('boton').addEventListener('click', (evento) => {
+    evento.stopPropagation();
+})
